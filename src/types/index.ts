a403a508a01a0cd7d7a91a58934d7102489035be
Wojciech Morawski <wojciech.morawski@ipfdigital.com>
2025-02-21 23:24:33 +0100

    fix(types): usePartialSetup

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,8 +2,9 @@ export type ActionCreators = Readonly<Record<string, (...args: any[]) => any>>;
 
 export type ActionDispatch = React.Dispatch<any>;
 
-export type CreateActionDispatch<AC extends ActionCreators> =
-  React.Dispatch<AC>;
+export type CreateActionDispatch<AC extends ActionCreators> = React.Dispatch<
+  GetActionTypes<AC>
+>;
 
 export type CreateBindedActions<AC extends ActionCreators> = {
   [K in keyof AC]: (...args: Parameters<AC[K]>) => void;
